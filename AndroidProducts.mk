b829ad676a20feda17a8449167dbd7493c9b231c
jefinhodatnt <jefiinhorelikia@gmail.com>
2020-11-26 04:21:43 -0300

    cherish initial bringup
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/cherish_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-   aosp_tulip-user \
-   aosp_tulip-userdebug \
-   aosp_tulip-eng
+   cherish_tulip-user \
+   cherish_tulip-userdebug \
+   cherish_tulip-eng
